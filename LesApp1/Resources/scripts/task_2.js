f20d055a8055d6e1ca23c9883750fe2a43cf877a
Bogdan <pinchuk.brus@windowslive.com>
2021-06-26 17:20:50 +0300

    corrected

--- LesApp1/Resources/scripts/task_2.js
+++ LesApp1/Resources/scripts/task_2.js
@@ -5,11 +5,28 @@ const Pi = 3.1415926535;
 let r = Number(prompt("Введите радиус цилиндра"));
 let h = Number(prompt("Введите высоту цилиндра"));
 
+let a = isNaN(r);
+let b = isNaN(h);
+
+
+// variant 1
+if (isNaN(r) && isNaN(h)) {
+    alert("Некоректный ввод значения.");
+} else if (r <= 0 || h <= 0) {
+    alert("Значение должно быть больше нуля!");
+} else {
+    V = Pi * r ** 2 * h;
+    alert(`Объем цилинда с высотой ${h} и радиуосом ${r} = ${V.toFixed(3)}`);
+}
+
+// variant 2
+/*
 if (!(Boolean(r) && Boolean(h))) {
     alert("Некоректный ввод значения.");
 } else if (r <= 0 || h <= 0) {
     alert("Значение должно быть больше нуля!");
 } else {
     V = Pi * r ** 2 * h;
-    alert(`Объем цилинда с высотой ${h} и радиуосом ${r} = ${V}`);
+    alert(`Объем цилинда с высотой ${h} и радиуосом ${r} = ${V.toFixed(3)}`);
 }
+*/
