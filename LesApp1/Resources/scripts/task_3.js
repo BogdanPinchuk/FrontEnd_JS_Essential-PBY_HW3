14af8f90b2d32ce39b41228a80bac2997fb1371d
Bogdan <pinchuk.brus@windowslive.com>
2021-06-26 17:56:31 +0300

    corrected task

--- LesApp1/Resources/scripts/task_3.js
+++ LesApp1/Resources/scripts/task_3.js
@@ -13,22 +13,41 @@ let productQuantity1 = Number(prompt(`Укажите количество про
 let isNotCorrectEnter = isNaN(productQuantity1) || isNaN(productQuantity2) || isNaN(productQuantity3),
     isLessThan0 = productQuantity1 < 0 || productQuantity2 < 0 || productQuantity3 < 0;
 
+// info message
+let str;
+
 if (isNotCorrectEnter) {
-    console.log("Некоректный ввод значения.");
-    alert("Некоректный ввод значения.");
+    str = "Некоректный ввод значения.";
+    console.log(str);
+    alert(str);
 } else if (isLessThan0) {
-    console.log("Значение должно быть больше нуля!");
-    alert("Значение должно быть больше нуля!");
+    str = "Значение должно быть больше нуля!";
+    console.log(str);
+    alert(str);
 } else {
     // Выведите на экран сколько пользователю нужно заплатить за каждый продукт по отедльности и всего за все продукты
     // используйте форматированный вывод
     let totalPriceProduct1 = productPrice1 * productQuantity1,
         totalPriceProduct2 = productPrice2 * productQuantity2,
         totalPriceProduct3 = productPrice3 * productQuantity3;
-
     let total = totalPriceProduct1 + totalPriceProduct2 + totalPriceProduct3;
-    console.log(`Общая сумма по продукту '${product1}' составляет ${totalPriceProduct1}`);
-    console.log(`Общая сумма по продукту '${product2}' составляет ${totalPriceProduct2}`);
-    console.log(`Общая сумма по продукту '${product3}' составляет ${totalPriceProduct3}`);
-    console.log("Всего " + total);
+
+    // доставка
+    let deliveryIsTrue = confirm("Необходима ли Вам доставка?");
+    if (deliveryIsTrue) {
+        total += 40;
+    }
+
+    str = `Общая сумма по продукту '${product1}' составляет ${totalPriceProduct1}\n` +
+        `Общая сумма по продукту '${product2}' составляет ${totalPriceProduct2}\n` +
+        `Общая сумма по продукту '${product3}' составляет ${totalPriceProduct3}\n`;
+
+    if (deliveryIsTrue) {
+        str += `Стоимость доставки: 40\n`
+    }
+
+    str += `Всего ${total}.`
+
+    console.log(str);
+    alert(str);
 }
